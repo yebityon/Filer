fbd102605b71dcd4325158f25c0a40e22ce3baa5
Taeyong Seong <prebell@outlook.jp>
2020-12-18 20:03:48 +0900

    update and fixes bug

--- FIler/string_filter.cpp
+++ FIler/string_filter.cpp
@@ -8,9 +8,16 @@
 #include "constant.hpp"
 static const std::string remove_string = "yebityon";
 static const std::string replace_string = "remutyan!";
+const std::string rpst = "/^(?:([A-Za-z]+):)?(\\/{0,3})([0-9.\\-A-Za-z]+)(?::(\\d+))?(?:\\/([^?#]*))?(?:\\?([^#]*))?(?:#(.*))?$/";
+std::regex pt{rpst};
 
 extern std::vector<std::string>urls;
 void extract_urls(const std::string s){
+    std::sregex_iterator end, ite{s.begin(), s.end(), pt};
+    for (; ite != end ; ++ite) {
+        if(std::find(ite->begin(),ite->end(),replace_string) != ite->end())
+            urls.push_back(ite->str());
+    }
     urls.emplace_back(s);
 }
 std::string filter(std::string s){
